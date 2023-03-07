df2ee457ac699f0ef1060c2011c1baf8d8348bee
proBarbonet <pro.barbonet@gmail.com>
2023-03-06 22:37:33 -0500

    Changes to work with Nuxt 3 part.2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
     app: {
-      baseURL: '/Louis_Portfoli0/'
+      baseURL: '/Louis_Portfoli0/',
+      buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
     }
 })
