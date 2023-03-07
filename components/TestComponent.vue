561fed19f46be230d445ed0eeca49adddb5c3617
proBarbonet <pro.barbonet@gmail.com>
2023-03-06 23:33:39 -0500

    Added a few components and 98.css style library

---
+++ components/TestComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="window" style="margin: 32px; width: 500px">
+        <div class="title-bar">
+            <div class="title-bar-text">
+                My First VB4 Program
+            </div>
+        </div>
+        <div class="window-body">
+            <p>Hola! Soy Dora!</p>
+        </div>
+    </div>
+  </template>
+
+<script lang="ts">
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,13 @@
+
+import TestComponent from '~~/components/TestComponent.vue';
+
 <template>
   <main>
     <div>
-      <h1>Home</h1>
+      <NowLookingForWorkBanner />
+      <h1>Hello there! My name is Louis and I’m a software developer from the greatest city of them all, Montreal.</h1>
       <NuxtLink to="/about">About</NuxtLink>
+      <TestComponent />
     </div>
   </main>
 </template>
